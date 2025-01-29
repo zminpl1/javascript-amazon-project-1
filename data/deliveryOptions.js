ef19c90e6d1ebe7728133f65d4cccc177067e21b
zminpl1 <Antoni2005ma@gmail.com>
2025-01-29 11:55:52 +0100

    Added interactiv for delivery option

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,13 +1,13 @@
 export const deliveryOptions = [{
     id: '1',
-    deliveryDays: 7,
+    duration: 7,
     priceCents: 0
 }, {
     id: '2',
-    deliveryDays: 3,
+    duration: 3,
     priceCents: 499
 }, {
     id: '3',
-    deliveryDays: 1,
+    duration: 1,
     priceCents: 999
-}]
+}];
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from '../data/cart.js';
+import { cart, removeFromCart, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -95,7 +95,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const isChecked = option.id === cartItem.deliveryOptionId;
 
     html +=
-    `<div class="delivery-option">
+    `<div class="delivery-option js-delivery-option"
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${option.id}">
         <input type="radio"
         ${isChecked ? 'checked' : ''}
         class="delivery-option-input"
@@ -114,8 +116,6 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     return html;
 }
 
-
-
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
 document.querySelectorAll('.js-delete-link').forEach((link) => {
@@ -125,5 +125,12 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
 
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
+    });
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+    element.addEventListener('click', () => {
+        const { productId, deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId, deliveryOptionId);
     });
 });
