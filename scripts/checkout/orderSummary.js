848c34e9657bd468be7ef8edecd4743020183e17
zminpl1 <Antoni2005ma@gmail.com>
2025-01-29 13:12:58 +0100

    Make paymentSummary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -6,8 +6,6 @@ import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 
-hello();
-
 export function renderOrderSummary() {
     let cartSummaryHTML = '';
 
