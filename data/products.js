aa0bfcfd8b48a7d9d8fb2f9e4ea2ad3b7d276413
zminpl1 <Antoni2005ma@gmail.com>
2025-01-31 02:06:55 +0100

    Learned how to use fetch in project and tests

--- data/products.js
+++ data/products.js
@@ -111,6 +111,34 @@ object3.method();
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+    ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    console.log(productsData);
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      } else if (productDetails.type === 'appliance') {
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });
+
+  return promise;
+}
+
+/*
+loadProductsFetch().then(() => {
+  console.log('next Step')
+});
+*/
+
 export function loadProducts(fun) {
   const request = new XMLHttpRequest();
 
--- test-jasmine/checkout/orderSummaryTest.js
+++ test-jasmine/checkout/orderSummaryTest.js
@@ -1,5 +1,5 @@
 import { loadFromStorage, cart } from "../../data/cart.js";
-import { loadProducts } from "../../data/products.js";
+import { loadProducts, loadProductsFetch } from "../../data/products.js";
 import { renderCheckoutHeader } from "../../scripts/checkout/checkoutHeader.js";
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { renderPaymentSummary } from "../../scripts/checkout/paymentSummary.js";
@@ -10,7 +10,7 @@ describe('test suite: renderOrderSummary', () => {
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
     beforeAll((done) => {
-        loadProducts(() => {
+        loadProductsFetch().then(() => {
             done();
         });
     });
