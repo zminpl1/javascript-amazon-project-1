7914ab8b8711af72f82045911215f50b391cebda
zminpl1 <Antoni2005ma@gmail.com>
2025-02-02 11:36:48 +0100

    Simple adjussment

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
-import {renderOrderSummary} from './checkout/orderSummary.js'
+import { renderOrderSummary } from './checkout/orderSummary.js'
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js'
